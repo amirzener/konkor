c9b494d203c5abaf1f3756b55fdc701cb3638a29
amirzener <amirzener2@gmail.com>
2025-09-07 12:53:51 +0330

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors());
